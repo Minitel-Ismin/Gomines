85fc981ea8b9069ceb98665dcbce0b23b5e47862
Guillaume ANDRES <guillaume.andres@yahoo.fr>
2017-11-24 19:19:38 +0100

    Nouveaux disques

--- src/Controller/FreeSpaceController.php
+++ src/Controller/FreeSpaceController.php
@@ -20,14 +20,12 @@ class FreeSpaceController extends AppController
      */
     public function index()
     {
-        $Disques["Films2"] = str_replace("%","",system("df -k | grep sdf2 | awk '{ print $5 }' | cut -c 1-3")); 
-        $Disques["Series2"] = str_replace("%","",system("df -k | grep sdd2 | awk '{ print $5 }' | cut -c 1-3")); 
-        $Disques["Jeux"] = str_replace("%","",system("df -k | grep sdh2 | awk '{ print $5 }' | cut -c 1-3")); 
+        $Disques["Jeux"] = str_replace("%","",system("df -k | grep sdb2 | awk '{ print $5 }' | cut -c 1-3")); 
         $Disques["Films1"] = str_replace("%","",system("df -k | grep sda2 | awk '{ print $5 }' | cut -c 1-3")); 
         $Disques["Series1"] = str_replace("%","",system("df -k | grep sdg2 | awk '{ print $5 }' | cut -c 1-3")); 
         $Disques["Blurays"] = str_replace("%","",system("df -k | grep sdb2 | awk '{ print $5 }' | cut -c 1-3")); 
         $Disques["NewDisk"] = str_replace("%","",system("df -k | grep sde1 | awk '{ print $5 }' | cut -c 1-3"));
-    
+        $Disques["Iron1"] = str_replace("%","",system("df -k | grep sdf1 | awk '{ print $5 }' | cut -c 1-3"));
         
         $this->set(compact('Disques'));
     }
